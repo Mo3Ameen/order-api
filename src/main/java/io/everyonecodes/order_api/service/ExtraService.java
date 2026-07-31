d027136701be3bb008cf96d68da14a50f5c310c0
Mo3Ameen <mohamad.syyy@gmail.com>
2026-07-31 11:14:24 +0200

    delete ExtraController and clean up ExtraService

--- src/main/java/io/everyonecodes/order_api/service/ExtraService.java
+++ src/main/java/io/everyonecodes/order_api/service/ExtraService.java
@@ -2,6 +2,7 @@ package io.everyonecodes.order_api.service;
 
 import io.everyonecodes.order_api.entity.Extra;
 import io.everyonecodes.order_api.entity.MenuItem;
+import io.everyonecodes.order_api.exception.ResourceNotFoundException;
 import io.everyonecodes.order_api.repository.ExtraRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +19,12 @@ public class ExtraService {
         this.repository = repository;
     }
 
-    public List<Extra> findAll() {
-        return repository.findAll();
-    }
-
     public List<Extra> findByMenuItemsContainingAndIsActive(MenuItem menuItem) {
         return repository.findByMenuItemsContainingAndIsActive(menuItem, true);
+    }
+
+    public List<Extra> findAll() {
+        return repository.findAll();
     }
 
     public Optional<Extra> findById(Long id) {
@@ -34,8 +35,32 @@ public class ExtraService {
         return repository.save(extra);
     }
 
+    public Extra updateExtra(Extra extra, Long id) {
+        Extra existingExtra = getExtra(id);
+        existingExtra.setName(extra.getName());
+        existingExtra.setIsActive(extra.getIsActive());
+        existingExtra.setPrice(extra.getPrice());
+        return save(existingExtra);
+    }
+
+    public Extra createExtra(Extra extra) {
+        extra.setId(null);
+        extra.setIsActive(extra.getIsActive() != null ? extra.getIsActive() : true);
+        return save(extra);
+    }
+
     public void deleteById(Long id) {
         repository.deleteById(id);
+    }
+
+    public void softDeleteById(Long id) {
+        Extra extra = getExtra(id);
+        extra.setIsActive(false);
+        save(extra);
+    }
+
+    private Extra getExtra(Long id) {
+        return findById(id).orElseThrow(() -> new ResourceNotFoundException("Extra " + id + " not found"));
     }
 
     public List<Extra> findAllById(Set<Long> extraIds) {
